612d797c1204d0e98fe7b3097f8a4cb2b3b55ed2
ajit0507 <62989333+ajit0507@users.noreply.github.com>
2023-01-30 16:00:01 +0530

    Update WelcomeController.java

--- src/main/java/com/example/demo/controller/WelcomeController.java
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -12,7 +12,7 @@ public class WelcomeController {
 		System.out.println("WelcomeController.getWelcome()");
 		System.out.println("WelcomeController.getWelcome()");
 
-		System.out.println("WelcomeController.getWelcome()2");
+		System.out.println("WelcomeController.getWelcome(3)");
 		return "welcome to docker CI CD pipleline work5";
 
 	}
