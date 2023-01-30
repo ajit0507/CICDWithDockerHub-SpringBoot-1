eb3c3c061a9adf67f4a545207a5be5a6063644e0
ajit0507 <ajitshivajipatil@gmail.com>
2023-01-30 15:58:29 +0530

    commit6

---
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MyController {
+	@GetMapping("/welcome")
+	public String getWelcome()
+	{
+		System.out.println("WelcomeController.getWelcome()");
+		System.out.println("WelcomeController.getWelcome()5");
+
+		return "welcome to docker CI CD pipleline work5";
+
+	}
+}
