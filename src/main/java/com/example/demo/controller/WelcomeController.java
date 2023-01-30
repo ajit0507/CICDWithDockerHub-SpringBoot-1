495b57a113bd262613e9920d74b629a4a445a68c
ajit0507 <ajitshivajipatil@gmail.com>
2023-01-30 13:41:04 +0530

    my third commit

---
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@GetMapping("/welcome")
+	public String getWelcome()
+	{
+		return "welcome to docker CI CD pipleline";
+	}
+	
+}
